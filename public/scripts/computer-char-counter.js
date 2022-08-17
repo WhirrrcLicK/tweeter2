dbcc48b8f906696d71059921d1939707ab1429f3
Kaiya Hedley <keshedley@gmail.com>
2022-08-17 17:21:21 -0600

    the counter is working and turns red when the character length goes beyond 140

---
+++ public/scripts/computer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function(event) {
+    const currentCount = $(this).val().length;
+
+    const counterElem = $(this).parent().children('.submit-tweet').children('.counter')
+    const remainingCount = 140 - currentCount;
+
+    counterElem.text(remainingCount)
+    if (remainingCount < 0) {
+      counterElem.css('color', '#d40e49');
+    }
+    else {
+      counterElem.css('color', '#37393b')
+    }
+  });
+});
