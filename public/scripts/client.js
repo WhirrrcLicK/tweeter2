eca379d3857fff12d917b2aeee6d6b32252e9f63
Kaiya Hedley <keshedley@gmail.com>
2022-08-18 20:56:24 -0600

    adding ajax functions and making tweet submissions dynamic rather than hardcoded.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,47 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+$(document).ready(function() {
+
+  $('#submit-tweet').submit(function(event) {
+    const tweetValue = $('#tweet-text').val()
+    event.preventDefault();
+    $.ajax('/tweets', {method: 'POST', data: $('#tweet-text').serialize()}).then(function() {loadTweets()})
+  })
+
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'}).then((result) => renderTweets(result))
+  }
+  loadTweets();
+
+ const renderTweets = function(tweets) {
+  let section = $(".all-tweets");
+  section.html(" ")
+  for (const tweet of tweets) {
+    section.prepend(createTweetElement(tweet))
+  };
+ };
+
+ const createTweetElement = function(tweet) {
+  let $tweet = `<section class = "tweet-box">
+  <header>
+  <div class="user-info">
+    <img src="${tweet.user.avatars}" class="avatar">
+    <div class="user">${tweet.user.name}</div>
+    <div class="user-handle">${tweet.user.handle}</div>
+    </div>
+  </header>
+  <div class="tweet-content">I fucked up the brownies.</div>
+  <footer class="tweet-footer">
+    <div class="tweet-age">10 days ago</div>
+    <div class="icons">
+    <i class="fa-solid fa-skull-crossbones"></i>
+    <i class="fa-solid fa-crow"></i>
+    <i class="fa-solid fa-cloud-moon"></i>
+    </div>
+  </footer>
+</section>`
+return $tweet;
+ }
+});
