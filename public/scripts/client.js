1c45d5d9a64e1da89d94d46361f4cd132e2c892c
Kaiya Hedley <keshedley@gmail.com>
2022-08-21 14:32:00 -0600

    final changes to layout, keeping text with no breaks in tweet-box, adding screenshots for readme

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,50 +8,60 @@
 $(document).ready(function() {
 
   $('#submit-tweet').submit(function(event) {
-    const tweetValue = $('#tweet-text').val()
+    const tweetValue = $('#tweet-text').val();
     event.preventDefault();
     if (tweetValue === null || tweetValue === "") {
-      return alert("You can't submit an empty tweet!")
+      $(".error-empty").slideDown("slow");
+      $(".error-exceeds").hide();
     } else if (tweetValue.length > 140) {
-      return alert("Too many characters!")
+      $(".error-exceeds").slideDown("slow");
+      $(".error-empty").hide();
     } else {
-    $.ajax('/tweets', {method: 'POST', data: $('#tweet-text').serialize()}).then(function() {loadTweets()})
-  }
-});
+      $.ajax('/tweets', {method: 'POST', data: $('#tweet-text').serialize()}).then(function() {
+        loadTweets();
+      });
+    }
+  });
+
+  const fetchInput = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
   const loadTweets = function() {
-    $.ajax('/tweets', {method: 'GET'}).then((result) => renderTweets(result))
-  }
+    $.ajax('/tweets', {method: 'GET'}).then((result) => renderTweets(result));
+  };
   loadTweets();
 
- const renderTweets = function(tweets) {
-  let section = $(".all-tweets");
-  section.html(" ")
-  for (const tweet of tweets) {
-    section.prepend(createTweetElement(tweet))
+  const renderTweets = function(tweets) {
+    let section = $(".all-tweets");
+    section.html(" ");
+    for (const tweet of tweets) {
+      section.prepend(createTweetElement(tweet));
+    }
   };
- };
 
- const createTweetElement = function(tweet) {
-  const timeAgo = timeago.format(new Date());
-  let $tweet = `<section class = "tweet-box">
-  <header>
-  <div class="user-info">
+  const createTweetElement = function(tweet) {
+    const timeAgo = timeago.format(tweet.created_at);
+    let $tweet = `<article class = "tweet-box">
+  <header class="tweet-header">
     <img src="${tweet.user.avatars}" class="avatar">
-    <div class="user">${tweet.user.name}</div>
-    <div class="user-handle">${tweet.user.handle}</div>
-    </div>
-  </header>
-  <div class="tweet-content">I fucked up the brownies.</div>
+    <p class="user">${tweet.user.name}</p>
+    <p class="user-handle">${tweet.user.handle}</p>
+    </header>
+    <div class="tweet-content">${fetchInput(tweet.content.text)}</div>
   <footer class="tweet-footer">
-    <div class="tweet-age">${timeAgo}</div>
+    <div class="tweet-age">
+    <time datetime="2022-08-11 18:00">${timeAgo}</time>
+    </div>
     <div class="icons">
     <i class="fa-solid fa-skull-crossbones"></i>
     <i class="fa-solid fa-crow"></i>
     <i class="fa-solid fa-cloud-moon"></i>
     </div>
   </footer>
-</section>`
-return $tweet;
- }
+</article>`;
+    return $tweet;
+  };
 });
--- public/scripts/computer-char-counter.js
+++ public/scripts/computer-char-counter.js
@@ -1,16 +1,34 @@
+// $(document).ready(function() {
+//   $('#tweet-text').on('keyup', function(event) {
+//     const currentCount = $(this).val().length;
+
+//     const counterElem = $(this).parent().children('.submit-tweet').children('.counter')
+//     const remainingCount = 140 - currentCount;
+
+//     counterElem.text(remainingCount)
+//     if (remainingCount < 0) {
+//       counterElem.css('color', '#d40e49');
+//     }
+//     else {
+//       counterElem.css('color', '#37393b')
+//     }
+//   });
+// });
+
 $(document).ready(function() {
-  $('#tweet-text').on('keyup', function(event) {
-    const currentCount = $(this).val().length;
-
-    const counterElem = $(this).parent().children('.submit-tweet').children('.counter')
-    const remainingCount = 140 - currentCount;
-
-    counterElem.text(remainingCount)
-    if (remainingCount < 0) {
-      counterElem.css('color', '#d40e49');
-    }
-    else {
-      counterElem.css('color', '#37393b')
-    }
-  });
+  $('#tweet-text').on('input', onInput);
 });
+
+const onInput = function(event) {
+  const currentCount = $(this).val().length;
+
+  const counterElem = $(this).parent().children('.submit-tweet').children('.counter');
+  const remainingCount = 140 - currentCount;
+
+  counterElem.text(remainingCount);
+  if (remainingCount < 0) {
+    counterElem.css('color', '#d40e49');
+  } else {
+    counterElem.css('color', '#37393b');
+  }
+};
